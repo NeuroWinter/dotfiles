1c770d3a81f2c09ef9271c6a9a0f1c0db38037d0
NeuroWinter <alex@pikori.com>
2025-07-22 14:48:26 +1200

    Update Neovim theme to rose pine

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -32,5 +32,30 @@ return {
       })
     end,
   },
-  { "dracula/vim", name = "dracula" }
+  { "dracula/vim", name = "dracula" },
+  {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+        require("rose-pine").setup({
+            variant = "moon", -- auto, main, moon, or dawn
+            dark_variant = "moon", -- main, moon, or dawn
+            dim_inactive_windows = true,
+            extend_background_behind_borders = true,
+
+            enable = {
+                terminal = true,
+                legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+                migrations = true, -- Handle deprecated options automatically
+            },
+
+            styles = {
+                bold = true,
+                italic = true,
+                transparency = true,
+            },
+        })
+        vim.cmd("colorscheme rose-pine-moon")
+	end
+  },
 }
